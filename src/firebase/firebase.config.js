634703b52091785bc36e5b8420c78f674b90f3ff
Sheuli Akter Shathy <sheuliakter85@gmail.com>
2022-10-18 19:04:51 +0600

    navbar and side nav add

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYKtT123Gk-NsOpHH-YZ9Ybpvb1AMnuQk",
+  authDomain: "dragon-news-c2c50.firebaseapp.com",
+  projectId: "dragon-news-c2c50",
+  storageBucket: "dragon-news-c2c50.appspot.com",
+  messagingSenderId: "1063122897099",
+  appId: "1:1063122897099:web:e976b04999f2f36104b34e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
